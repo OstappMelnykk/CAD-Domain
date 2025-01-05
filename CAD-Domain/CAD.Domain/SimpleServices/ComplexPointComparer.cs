2648c3a8bae58ab6b6457e70f88a228eb03340be
OstappMelnykk <79ostap@ukr.net>
2025-01-05 20:01:00 +0200

    Add ComplexPoint comparer and global sorting service

---
+++ CAD-Domain/CAD.Domain/SimpleServices/ComplexPointComparer.cs
@@ -0,0 +1,25 @@
+﻿using CAD.Domain.ComplexFigures;
+
+namespace CAD.Domain.SimpleServices;
+
+public class ComplexPointComparer : IComparer<ComplexPoint>
+{
+    public int Compare(ComplexPoint? p1, ComplexPoint? p2)
+    {
+        if (p1 is null || p2 is null)
+        {
+            return p1 is null ? (p2 is null ? 0 : -1) : 1;
+        }
+
+        // Спочатку порівнюємо координату Y
+        int comparison = p1.SimplePoint.Y.CompareTo(p2.SimplePoint.Y);
+        if (comparison != 0) return comparison;
+
+        // Якщо Y однаковий, порівнюємо Z
+        comparison = p1.SimplePoint.Z.CompareTo(p2.SimplePoint.Z);
+        if (comparison != 0) return comparison;
+
+        // Якщо Y і Z однакові, порівнюємо X
+        return p1.SimplePoint.X.CompareTo(p2.SimplePoint.X);
+    }
+}
---
+++ CAD-Domain/CAD.Domain/SimpleServices/SortingService.cs
@@ -0,0 +1,9 @@
+﻿using CAD.Domain.ComplexFigures;
+
+namespace CAD.Domain.SimpleServices;
+
+public static class SortingService
+{
+    public static void GlobalSorting(List<ComplexPoint> points) =>
+        points.Sort(new ComplexPointComparer());
+}
