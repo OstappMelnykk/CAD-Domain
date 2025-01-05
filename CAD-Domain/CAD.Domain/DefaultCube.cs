502b3c9b88869dedde90dda76e03266510aad9a1
OstappMelnykk <79ostap@ukr.net>
2025-01-05 20:45:00 +0200

    Add default cube representation

---
+++ CAD-Domain/CAD.Domain/DefaultCube.cs
@@ -0,0 +1,80 @@
+﻿using CAD.Domain.ComplexFigures;
+
+namespace CAD.Domain;
+
+public static class DefaultCube
+{
+    private static readonly List<ComplexPoint> DefaultCube8Points = new()
+    {
+        new ComplexPoint(new Simple3DPoint(-1, -1, -1)),
+        new ComplexPoint(new Simple3DPoint(1, -1, -1)),
+        new ComplexPoint(new Simple3DPoint(-1, -1, 1)),
+        new ComplexPoint(new Simple3DPoint(1, -1, 1)),
+        new ComplexPoint(new Simple3DPoint(-1, 1, -1)),
+        new ComplexPoint(new Simple3DPoint(1, 1, -1)),
+        new ComplexPoint(new Simple3DPoint(-1, 1, 1)),
+        new ComplexPoint(new Simple3DPoint(1, 1, 1)),
+    };
+    
+    private static readonly List<ComplexPoint> DefaultCube12MiddlePoints = new()
+    {
+        new ComplexPoint(new Simple3DPoint(0, -1, -1)),
+        new ComplexPoint(new Simple3DPoint(-1, -1, 0)),
+        new ComplexPoint(new Simple3DPoint(1, -1, 0)),
+        new ComplexPoint(new Simple3DPoint(0, -1, 1)),
+        new ComplexPoint(new Simple3DPoint(-1, 0, -1)),
+        new ComplexPoint(new Simple3DPoint(1, 0, -1)),
+        new ComplexPoint(new Simple3DPoint(-1, 0, 1)),
+        new ComplexPoint(new Simple3DPoint(1, 0, 1) ),
+        new ComplexPoint(new Simple3DPoint(0, 1, -1)),
+        new ComplexPoint(new Simple3DPoint(-1, 1, 0)),
+        new ComplexPoint(new Simple3DPoint(1, 1, 0)),
+        new ComplexPoint(new Simple3DPoint(0, 1, 1)),
+    };
+    
+    private static readonly List<ComplexPoint> DefaultCube20Points = new()
+    {
+         new ComplexPoint(new Simple3DPoint(-1, -1, -1)),
+         new ComplexPoint(new Simple3DPoint(0, -1, -1)),
+         new ComplexPoint(new Simple3DPoint(1, -1, -1)),
+         new ComplexPoint(new Simple3DPoint(-1, -1, 0)),
+         new ComplexPoint(new Simple3DPoint(1, -1, 0)),
+         new ComplexPoint(new Simple3DPoint(-1, -1, 1)),
+         new ComplexPoint(new Simple3DPoint(0, -1, 1)),
+         new ComplexPoint(new Simple3DPoint(1, -1, 1)),
+         new ComplexPoint(new Simple3DPoint(-1, 0, -1)),
+         new ComplexPoint(new Simple3DPoint(1, 0, -1)),
+         new ComplexPoint(new Simple3DPoint(-1, 0, 1)),
+         new ComplexPoint(new Simple3DPoint(1, 0, 1)),
+         new ComplexPoint(new Simple3DPoint(-1, 1, -1)),
+         new ComplexPoint(new Simple3DPoint(0, 1, -1)),
+         new ComplexPoint(new Simple3DPoint(1, 1, -1)),
+         new ComplexPoint(new Simple3DPoint(-1, 1, 0)),
+         new ComplexPoint(new Simple3DPoint(1, 1, 0)),
+         new ComplexPoint(new Simple3DPoint(-1, 1, 1)),
+         new ComplexPoint(new Simple3DPoint(0, 1, 1)),
+         new ComplexPoint(new Simple3DPoint(1, 1, 1)),
+    };
+    
+    
+    public static List<ComplexPoint> GetDefaultCube8Points()
+    {
+        return DefaultCube8Points
+            .Select(point => (ComplexPoint)point.Clone())
+            .ToList();
+    }
+
+    public static List<ComplexPoint> GetDefaultCube12MiddlePoints()
+    {
+        return DefaultCube12MiddlePoints
+            .Select(point => (ComplexPoint)point.Clone())
+            .ToList();
+    }
+
+    public static List<ComplexPoint> GetDefaultCube20Points()
+    {
+        return DefaultCube20Points
+            .Select(point => (ComplexPoint)point.Clone())
+            .ToList();
+    }
+}
