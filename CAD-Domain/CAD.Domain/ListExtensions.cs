00892cc8d36c669321734ac7793607db66556f04
OstappMelnykk <79ostap@ukr.net>
2025-01-06 13:49:00 +0200

    Add ListExtensions for deep cloning lists with extension method
       This extension method supports deep copying of lists using ICloneable,
       ensuring each object in the list is cloned individually.

---
+++ CAD-Domain/CAD.Domain/ListExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CAD.Domain;
+
+public static class ListExtensions
+{
+    public static List<T> DeepClone<T>(this List<T> list) where T : ICloneable
+    {
+        return list.Select(item => (T)item.Clone()).ToList();
+    }
+}
