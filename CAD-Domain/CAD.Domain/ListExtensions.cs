439bb0620671463479de5b9738335ad4c7c42b60
OstappMelnykk <79ostap@ukr.net>
2025-01-09 10:18:00 +0200

    Add extension method to distinct ComplexPoint list by SimplePoint and merge LocalIds
    The method groups ComplexPoint objects by SimplePoint
    Merges LocalIds from all ComplexPoint objects in the same group, ensuring uniqueness
    Creates new ComplexPoint objects with merged LocalIds and properties from the first item in the group

--- CAD-Domain/CAD.Domain/ListExtensions.cs
+++ CAD-Domain/CAD.Domain/ListExtensions.cs
@@ -1,9 +1,28 @@
-﻿namespace CAD.Domain;
+﻿using CAD.Domain.ComplexFigures;
+
+namespace CAD.Domain;
 
 public static class ListExtensions
 {
     public static List<T> DeepClone<T>(this List<T> list) where T : ICloneable
     {
         return list.Select(item => (T)item.Clone()).ToList();
+    }
+    
+    public static List<ComplexPoint> DistinctAndMergeLocalIds(this List<ComplexPoint> complexPoints)
+    {
+        return complexPoints
+            .GroupBy(cp => cp.SimplePoint) // Group by SimplePoint
+            .Select(group =>
+            {
+                // Merge LocalIds from all ComplexPoints in the group
+                var mergedLocalIds = group.SelectMany(cp => cp.LocalIds).Distinct().ToList();
+                //var mergedLocalIds = group.SelectMany(cp => cp.LocalIds).ToList();
+
+                // Create a new ComplexPoint with the merged LocalIds and the properties of the first item in the group
+                var first = group.First();
+                return new ComplexPoint(first.SimplePoint, first.GlobalId, mergedLocalIds);
+            })
+            .ToList();
     }
 }
