e25162f6caa086dfe33a5a3918f9ba301184d46a
OstappMelnykk <79ostap@ukr.net>
2025-01-11 23:34:03 +0200

    Add an example of using SuperCubeService

--- CAD-Domain/CAD.ConsoleApplication/Program.cs
+++ CAD-Domain/CAD.ConsoleApplication/Program.cs
@@ -7,6 +7,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(new SuperCube(2, 2, 2).GetJsonSerializedCube());
+        SuperCube sc = new SuperCube();
+        SuperCubeService scs = new SuperCubeService(sc);
+        
+        Console.WriteLine("Default SuperCube initialization with division: {x: 1, y: 1, z: 1}");
+        Console.WriteLine("Points: " + scs.GetPoints().Count);
+        Console.WriteLine("Pairs of indices: " + scs.PairsToConnect_asListIndices.Count);
+        Console.WriteLine("Polygons: " + scs.Polygons_asListIndices.Count);
+        
+        scs.DivideCubeByAxes(2, 2, 2);
+        Console.WriteLine("Devide: {x: 2, y: 2, z: 2}");
+        Console.WriteLine("Points: " + scs.GetPoints().Count);
+        Console.WriteLine("Pairs of indices: " + scs.PairsToConnect_asListIndices.Count);
+        Console.WriteLine("Polygons: " + scs.Polygons_asListIndices.Count);
+        
+        scs.DivideCubeByAxes(3, 3, 3);
+        Console.WriteLine("Devide: {x: 3, y: 3, z: 3}");
+        Console.WriteLine("Points: " + scs.GetPoints().Count);
+        Console.WriteLine("Pairs of indices: " + scs.PairsToConnect_asListIndices.Count);
+        Console.WriteLine("Polygons: " + scs.Polygons_asListIndices.Count);
     }
 }
