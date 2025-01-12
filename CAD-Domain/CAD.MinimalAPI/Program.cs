a75e37a8a78d3217048c4a2cf5dc7f5aafa943dc
OstappMelnykk <79ostap@ukr.net>
2025-01-12 02:14:30 +0200

    Add simple logic for minimal Web API

--- CAD-Domain/CAD.MinimalAPI/Program.cs
+++ CAD-Domain/CAD.MinimalAPI/Program.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using CAD.Domain.ComplexFigures;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CAD.MinimalAPI;
 
@@ -18,6 +20,7 @@ public class Program
             });
         });
         
+        builder.Services.AddAuthorization();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddSingleton<SuperCubeService>();
@@ -46,6 +49,61 @@ public class Program
         
         app.UseAuthorization();
 
+
+        app.MapGet("/", () => "Hello World!");
+        
+        app.MapPost("/Devide", ([FromBody] DevideDTO devideDto, [FromServices] SuperCubeService scs) =>
+        {
+            var stopwatch = Stopwatch.StartNew();
+            scs.DivideCubeByAxes(devideDto.x, devideDto.y, devideDto.z);
+            stopwatch.Stop();
+
+            return Results.Ok(new 
+            { 
+                devideDto.x, 
+                devideDto.y, 
+                devideDto.z, 
+                executionTime = stopwatch.Elapsed.TotalSeconds  
+            });
+        });
+
+        app.MapGet("/GetPoints", (SuperCubeService scs) => 
+        {
+            return Results.Ok(new 
+            { 
+                Count = scs.GetPoints().Count, 
+                Points = scs.GetPoints()
+            });
+        });
+        
+        app.MapGet("/GetPairsOfIndices", (SuperCubeService scs) =>
+        {
+           List<int[]> indixes = 
+                           scs.PairsToConnect_asListIndices.Select(pair => 
+                                           new int[2] { pair.Item1, pair.Item2 }).ToList();
+            return Results.Ok(new 
+            { 
+                Count = indixes.Count, 
+                Indices = indixes 
+            });
+        });
+        
+        
+        app.MapGet("/GetPolygons", (SuperCubeService scs) => 
+        {
+            List<int[]> poligons =
+                            scs.Polygons_asListIndices.Select(pair =>
+                                            new int[3] { pair.Item1, pair.Item2, pair.Item3 }).ToList();
+            return Results.Ok(new 
+            { 
+                Count = poligons.Count, 
+                Poligons = poligons
+            });
+        });
+        
+        
         app.Run();
     }
 }
+
+public record DevideDTO(int x, int y, int z);
